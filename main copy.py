efba4255c4845bfb9792ed98b22bd2cc2e2c86c2
Felipe Muñoz <felipe.munoz@orion.global>
2025-12-28 12:48:48 -0300

    changes
    Signed-off-by: Felipe Muñoz <felipe.munoz@orion.global>

---
+++ main copy.py
@@ -0,0 +1,51 @@
+from typing import List
+
+from pydantic import BaseModel, Field
+from dotenv import load_dotenv
+
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_tavily import TavilySearch
+from langchain_google_genai import ChatGoogleGenerativeAI
+import os
+
+
+load_dotenv(r"C:\Users\FelipeMuñozVargas\langchain-course_agents\.env")
+
+
+class Source(BaseModel):
+    """Schema for a source used by the agent"""
+
+    url: str = Field(description="The URL of the source")
+
+
+class AgentResponse(BaseModel):
+    """Schema for agent response with answer and sources"""
+
+    answer: str = Field(description="Thr agent's answer to the query")
+    sources: List[Source] = Field(
+        default_factory=list, description="List of sources used to generate the answer"
+    )
+
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash-lite", temperature=0)
+tools = [TavilySearch()]
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
+
+
+
+def main():
+    print("Hello from langchain-course!")
+    result = agent.invoke(
+        {
+            "messages": HumanMessage(
+                content="search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details?"
+            )
+        }
+    )
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -1,38 +1,49 @@
+from typing import List
+
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv
+load_dotenv(r"C:\Users\FelipeMuñozVargas\langchain-course_agents\.env")
 
-load_dotenv()
 
-from langchain.agents import create_agent
-from langchain_openai import ChatOpenAI
+from langchain_classic.agents import AgentExecutor
+from langchain_classic.agents import create_react_agent
+from langchain_core.output_parsers.pydantic import PydanticOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_tavily import TavilySearch
 
+from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
 from schemas import AgentResponse
 
 tools = [TavilySearch()]
-llm = ChatOpenAI(model="gpt-4o")
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash-lite", temperature=0)
+output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+
+react_prompt_with_format_instructions = PromptTemplate(
+    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+    input_variables=["input", "agent_scratchpad", "tool_names"],
+).partial(format_instructions=output_parser.get_format_instructions())
 
 
-agent = create_agent(
-    model=llm,
+agent = create_react_agent(
+    llm=llm,
     tools=tools,
-    response_format=AgentResponse,
+    prompt=react_prompt_with_format_instructions,
 )
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+extract_output = RunnableLambda(lambda x: x["output"])
+parse_output = RunnableLambda(lambda x: output_parser.parse(x))
+chain = agent_executor | extract_output | parse_output
 
 
 def main():
-    result = agent.invoke(
-        {
-            "messages": [
-                {
-                    "role": "user",
-                    "content": "search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details",
-                }
-            ]
+    result = chain.invoke(
+        input={
+            "input": "search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details",
         }
     )
-    # Access structured response from the agent
-    structured = result.get("structured_response", None)
-    print(structured if structured is not None else result)
+    print(result)
 
 
 if __name__ == "__main__":
